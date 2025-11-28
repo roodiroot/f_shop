ee8d2ab87b874e6fa545cb1e620649a372e99638
maxim <borisov130490@gmail.com>
2025-11-28 16:00:41 +0300

    add nodemailer

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,7 +1,24 @@
-export default () => ({
+export default ({ env }) => ({
   graphql: {
     config: {
       landingPage: true, // Включает Sandbox в продакшне (НЕ рекомендуется)
+    },
+  },
+  email: {
+    config: {
+      provider: "nodemailer",
+      providerOptions: {
+        host: env("BEGET_EMAIL_HOST"),
+        port: env("BEGET_EMAIL_PORT"),
+        auth: {
+          user: env("BEGET_EMAIL_USER"),
+          pass: env("BEGET_EMAIL_PASS"),
+        },
+      },
+      settings: {
+        defaultFrom: env("BEGET_EMAIL_USER"),
+        defaultReplyTo: env("BEGET_EMAIL_USER"),
+      },
     },
   },
 });
